86a0468ec878bea0f845d7715ccdb8e62627a705
Ritik <ara.kumarritik>
2024-03-05 12:17:04 +0530

    rk commit

---
+++ app.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from flask import Flask, jsonify, request
+import pickle
+
+# load model
+model = pickle.load(open('model.pkl','rb'))
+
+# app
+app = Flask(__name__)
+
+# routes
+@app.route('/', methods=['POST'])
+def index():
+    return render_templ
+
+# def predict():
+#     # get data
+#     data = request.get_json(force=True)
+
+#     # convert data into dataframe
+#     data.update((x, [y]) for x, y in data.items())
+#     data_df = pd.DataFrame.from_dict(data)
+
+#     # predictions
+#     result = model.predict(data_df)
+
+#     # send back to browser
+#     output = {'results': int(result[0])}
+
+#     # return data
+#     if int(result[0]) == 0:
+#            return jsonify(results="Motor In Good Condition")
+#     elif int(result[0]) == 1:
+#         return jsonify(results="Motor In Bad Condition")
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
